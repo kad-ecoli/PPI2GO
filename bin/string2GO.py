7079649be0dcdb79cfddc9714b5d1ac806655f1d
zcx <zcx@umich.edu>
2016-11-02 11:11:50 -0400

    add gwGOfreq and swGOfreq

--- bin/string2GO.py
+++ bin/string2GO.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 docstring='''string2GO.py P75994
-    predict GO from STRING protein-protein interaction network
+    predict GO from STRING protein-protein interaction network.
+    
+    entry    confidence_of_interaction  GO_term   confidence_of_GO
+    partner1             991           GO:0000001        5
+    partner2             356           GO:0000001        2
+    partner3             474           GO:0000002        2
+
+    confidence of interaction is in [0,1000]
+    confidence of GO is in [1,5]
+
+Confidence Score of GO:0000001
+[1] GOfreq (number of partners annotated with GO divided by partner number):
+    Cscore(GO:0000001)=2/3
+[2] swGOfreq (averaged confidence of interaction):
+    Cscore(GO:0000001)=(0.991+0.356)/2
+[3] gwGOfreq (averaged confidence of GO annotation):
+    Cscore(GO:0000001)=(5/5+2/5)/2
 
 options:
     -excludeGO=GO:0005515,GO:0005488,GO:0003674,GO:0008150,GO:0005575
@@ -18,7 +34,7 @@ datdir=os.path.abspath(os.path.dirname(__file__)+"/../dat")
 
 full_uniprot_2_string=os.path.join(datdir,"full_uniprot_2_string.tsv.gz")
 protein_links=os.path.join(datdir,"protein.links.txt.gz")
-all_go_knowledge_explicit=os.path.join(datdir,"all_go_knowledge_explicit.tsv.gz")
+all_go_knowledge_explicit=os.path.join(datdir,"go_explicit.tsv.gz")
 
 def uniprot2string(uniprotID="P75994"):
     '''map uniprot ID to a list of matching string ID'''
@@ -27,32 +43,39 @@ def uniprot2string(uniprotID="P75994"):
     stdout,stderr=p.communicate()
     if not stdout.strip():
         return ''
-    stringID_list=[line.split()[2] for line in stdout.splitlines()]
+    # reformat full_uniprot_2_string so that it only contains two column
+    stringID_list=[line.split()[1] for line in stdout.splitlines()]
     return stringID_list
 
 def get_PPI_partners(stringID_list=["b1167","4062745","ECDH10B_1216"]):
-    '''get a list of interaction partners'''
-    partner_list=[]
+    '''get a dict whose key is interaction partners and value is combined_score'''
+    partner_dict=dict()
+    if not stringID_list:
+        return partner_dict
     cmd='zgrep "\\b%s\\b" %s'%("\\|".join(stringID_list),protein_links)
     p=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
     stdout,stderr=p.communicate()
     if not stdout.strip():
-        return partner_list
+        return partner_dict
     for line in stdout.splitlines():
-        line=[e.split('.')[1] for e in line.split()[:2]]
-        if line[0] in stringID_list:
-            partner_list.append(line[1])
-        elif line[1] in stringID_list:
-            partner_list.append(line[0])
-    return partner_list
+        line=line.split()
+        combined_score=float(line[2])/100.
+        stringID_0=line[0].split('.')[-1]
+        stringID_1=line[1].split('.')[-1]
+        if stringID_0 in stringID_list:
+            partner_dict[stringID_1]=combined_score
+        elif stringID_1 in stringID_list:
+            partner_dict[stringID_0]=combined_score
+    return partner_dict
 
-def get_partner_terms(partner_list=[],excludeGO='',obo_dict=dict()):
+def get_partner_terms(partner_dict=dict(),excludeGO='',obo_dict=dict()):
     '''get mapping from GO terms to string ID'''
     partner2GO_dict={"F":dict(),"P":dict(),"C":dict()}
     if isinstance(excludeGO,str):
         excludeGO=excludeGO.split(',')
     excludeGO=set(excludeGO)
-    cmd='zgrep "\\b%s\\b" %s'%("\\|".join(partner_list),all_go_knowledge_explicit)
+    cmd='zgrep "\\b%s\\b" %s'%("\\|".join(partner_dict.keys()),
+        all_go_knowledge_explicit)
     p=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
     stdout,stderr=p.communicate()
     if not stdout.strip():
@@ -60,41 +83,64 @@ def get_partner_terms(partner_list=[],excludeGO='',obo_dict=dict()):
 
     ## map GO term (and parent terms) to partners ##
     for line in stdout.splitlines():
-        line=line.split()
-        partner_ID=line[1]
-        Term=line[3]
-        if not partner_ID in partner_list or Term in excludeGO:
+        line=line.split() # stringID GOterm cscore
+        partner_ID=line[0]
+        Term=line[1]
+        Cscore=(float(line[2])+5)/10
+        if not partner_ID in partner_dict or Term in excludeGO:
             continue
         for Aspect in partner2GO_dict:
             if not Term in obo_dict[Aspect]["Term"]:
                 continue
-            Term_list=obo_dict.is_a(
-                Term,direct=False,name=False,number=False).split()
+            Term_list=list(set(obo_dict.is_a(Term,direct=False,
+                name=False,number=False).split())-excludeGO)
             if not partner_ID in partner2GO_dict:
-                partner2GO_dict[Aspect][partner_ID]=Term_list
-            else:
-                partner2GO_dict[Aspect][partner_ID]+=Term_list
+                partner2GO_dict[Aspect][partner_ID]=dict()
 
-    ## remove repeative GO terms ##
-    for Aspect in partner2GO_dict:
-        for partner_ID in partner2GO_dict[Aspect]:
-             partner2GO_dict[Aspect][partner_ID]=list(
-                set(partner2GO_dict[Aspect][partner_ID])-excludeGO)
+            for Term in Term_list:
+                if not Term in partner2GO_dict[Aspect][partner_ID] or \
+                    partner2GO_dict[Aspect][partner_ID][Term]<Cscore:
+                    partner2GO_dict[Aspect][partner_ID][Term]=Cscore
     return partner2GO_dict
 
-def partner2GOfreq(Aspect='F',partner2GO_dict=dict()):
-    '''get final confidence score for a specific Aspect'''
-    GOfreq_pred=''
+def partner2GOfreq(Aspect='F',partner_dict=dict(),partner2GO_dict=dict()):
+    '''get final confidence score for a specific Aspect by following scoring:
+    GOfreq, swGOfreq, gwGOfreq, dwGOfreq
+    '''
+    GOfreq_pred_list=[]
+    swGOfreq_pred_list=[]
+    gwGOfreq_pred_list=[]
 
     GO_list_cat=[] # list of all GO terms annotated to all partners
-    for partner_ID in partner2GO_dict[Aspect]:
-        GO_list_cat+=partner2GO_dict[Aspect][partner_ID]
+    for partner_ID in partner2GO_dict[Aspect].keys():
+        GO_list_cat+=partner2GO_dict[Aspect][partner_ID].keys()
+    GO_list_cat=list(set(GO_list_cat)) # non-redundant list
     
-    for Term in set(GO_list_cat):
-        cscore=1.*sum([(Term in partner2GO_dict[Aspect][partner_ID]) for \
-            partner_ID in partner2GO_dict[Aspect]])/len(partner2GO_dict[Aspect])
-        GOfreq_pred+="%s\t%s\t%.2f\n"%(Term,Aspect,cscore)
-    return GOfreq_pred
+    for Term in GO_list_cat:
+        GOfreq_cscore=sum([(1. \
+            if Term in partner2GO_dict[Aspect][partner_ID] else 0.
+            ) for partner_ID in partner2GO_dict[Aspect]]
+            )/len(partner2GO_dict[Aspect])
+        swGOfreq_cscore=sum([(partner_dict[partner_ID] \
+            if Term in partner2GO_dict[Aspect][partner_ID] else 0.
+            ) for partner_ID in partner2GO_dict[Aspect]]
+            )/len(partner2GO_dict[Aspect])
+        gwGOfreq_cscore=sum([(partner2GO_dict[Aspect][partner_ID][Term] \
+            if Term in partner2GO_dict[Aspect][partner_ID] else 0.
+            ) for partner_ID in partner2GO_dict[Aspect]]
+            )/len(partner2GO_dict[Aspect])
+
+        GOfreq_pred_list.append((GOfreq_cscore,Term,Aspect))
+        swGOfreq_pred_list.append((swGOfreq_cscore,Term,Aspect))
+        gwGOfreq_pred_list.append((gwGOfreq_cscore,Term,Aspect))
+    
+    GOfreq_pred=''.join(["%s\t%s\t%.3f\n"%(Term,Aspect,cscore) for \
+        cscore,Term,Aspect in sorted(GOfreq_pred_list,reverse=True)])
+    swGOfreq_pred=''.join(["%s\t%s\t%.3f\n"%(Term,Aspect,cscore) for \
+        cscore,Term,Aspect in sorted(swGOfreq_pred_list,reverse=True)])
+    gwGOfreq_pred=''.join(["%s\t%s\t%.3f\n"%(Term,Aspect,cscore) for \
+        cscore,Term,Aspect in sorted(gwGOfreq_pred_list,reverse=True)])
+    return GOfreq_pred,swGOfreq_pred,gwGOfreq_pred
 
 if __name__=="__main__":
     excludeGO="GO:0005515,GO:0005488,GO:0003674,GO:0008150,GO:0005575"
@@ -113,7 +159,6 @@ if __name__=="__main__":
         sys.stderr.write(docstring)
         exit()
 
-
     #### parse GO hierachy ####
     fp=open(wget(obo_url,show_url=True),'rU')
     obo_txt=fp.read()
@@ -123,13 +168,24 @@ if __name__=="__main__":
     stringID_list=uniprot2string(argv[0])
     sys.stdout.write("mapped to string ID: %s\n"%(','.join(stringID_list)))
 
-    partner_list=get_PPI_partners(stringID_list)
-    sys.stdout.write("mapped to %d partners\n"%len(partner_list))
+    partner_dict=get_PPI_partners(stringID_list)
+    sys.stdout.write("mapped to %d partners\n"%len(partner_dict))
 
-    partner2GO_dict=get_partner_terms(partner_list,excludeGO,obo_dict)
+    partner2GO_dict=get_partner_terms(partner_dict,excludeGO,obo_dict)
     for Aspect in ["MF","BP","CC"]:
         sys.stdout.write("Calculating GO-%s from %d partners\n"%(Aspect,
             len(partner2GO_dict[Aspect[1]])))
+        GOfreq_pred,swGOfreq_pred,gwGOfreq_pred=partner2GOfreq(
+            Aspect[1],partner_dict,partner2GO_dict)
+
         fp=open("string_GOfreq_"+Aspect,'w')
-        fp.write(partner2GOfreq(Aspect[1],partner2GO_dict))
+        fp.write(GOfreq_pred)
+        fp.close()
+
+        fp=open("string_swGOfreq_"+Aspect,'w')
+        fp.write(swGOfreq_pred)
+        fp.close()
+
+        fp=open("string_gwGOfreq_"+Aspect,'w')
+        fp.write(gwGOfreq_pred)
         fp.close()
